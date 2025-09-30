9c120b769af54cdb8df720816b69e94888b0ceda
rawcode445-sys <raw.code445@gmail.com>
2025-09-30 10:20:48 +0200

    Update guards.js

--- assets/js/guards.js
+++ assets/js/guards.js
@@ -4,8 +4,7 @@ async function sha256(txt){
   return [...new Uint8Array(buf)].map(b=>b.toString(16).padStart(2,'0')).join('');
 }
 
-const GATE1_HASH = "1a79a4d60de6718e8e5b326e338ae533f9a4f40fa0c0f4eb927f0b02c6f7e97e
-";
+const GATE1_HASH = "1a79a4d60de6718e8e5b326e338ae533f9a4f40fa0c0f4eb927f0b02c6f7e97e";
 
 
 async function checkGate(input, expectedHash){
